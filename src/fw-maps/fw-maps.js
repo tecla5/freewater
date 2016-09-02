15cd6e0773d8e2565b2c3d528413143043d08ca5
jcc2303 <cabrera.javier@gmail.com>
2016-09-02 15:57:38 +0200

    added maps and firebase

---
+++ src/fw-maps/fw-maps.js
@@ -0,0 +1,200 @@
+function changeToArray(obj){
+  var array = [];
+
+  if (obj){
+    for(var propt in obj){
+      array.push(obj[propt]);
+    }
+  }
+
+  return array;
+}
+
+function search(array, element){
+  for (var i = 0; i < array.length; i++){
+    if (array[i] === element){
+      return array[i];
+    }
+  }
+
+  return null;
+}
+
+function  userDontHaveOpinion(mark, userId){
+  console.log('mark.confirms', mark.confirms);
+  console.log('mark.complaints', mark.complaints);
+  console.log('userId', userId);
+
+  var found = search(mark.confirms, userId);
+
+  if (!found){
+    found = search(mark.complaints, userId);
+  }
+
+  console.log('found', found);
+  return !found;
+}
+
+Polymer({
+  is: 'fw-maps',
+  properties:{
+    usersReady: Boolean,
+    marks: {
+      type: Array,
+      observer: 'dataChanged'
+    }
+  },
+  observers: [
+    'loadMarksToMap(marks, usersReady)'
+  ],
+  ready: function(){
+    console.log('free-water-map');
+    var self =  this;
+
+    //this.marksdataSource = new Firebase('https://free-water.firebaseio.com/marks');
+    this.marksdataSource = [];
+
+    // TODO uncomment
+    /*
+    this.$$('smart-map').addEventListener('country-changed', this.loadMarks.bind(this));
+    this.$$('smart-map').addEventListener('publish', this.publish.bind(this));
+    this.$$('smart-map').addEventListener('confirm', function(data){
+      self.addOpinion('confirms', data);
+    });
+
+    this.$$('smart-map').addEventListener('complaint', function(data){
+      self.addOpinion('complaints', data);
+    });
+    */
+
+    /*
+    this.$$('freewater-users').addEventListener('users-ready', function(){
+      self.usersReady = true;
+    });
+    */
+  },
+  addOpinion: function (typeOpinion, data) {
+    var message = 'Yo have to login to confirm or deny a water point';
+
+    this.checkLogin(message).then(function(user){
+        var mark = data.detail.mark;
+
+        if (userDontHaveOpinion(mark, user.id)){
+          //var marksdataSource = new Firebase('https://free-water.firebaseio.com/marks/' +
+          //  mark.__firebaseKey__);
+          var array = null;
+          //marksdataSource.child(typeOpinion);
+
+          if (!array){
+            array = [];
+          }
+
+          array.push(user.id);
+          var objectToUpdate = {};
+          objectToUpdate[typeOpinion] = array;
+          //TODO: uncomment
+          //marksdataSource.update( objectToUpdate );
+        }
+      });
+  },
+  loadMarks: function(customEvent){
+
+    var country = customEvent.detail.country;
+    console.log('country', country);
+    var self = this;
+
+    /* TODO:  uncomment
+    this.marksdataSource.orderByChild('country').equalTo(country).on('value', function(snapshot) {
+      self.marks = snapshot.val();
+
+        if (self.$$('.progress-panel').style.display === 'block'){
+          self.hideProgressbar();
+        }
+
+      }, function (errorObject) {
+        console.log('The read failed: ' + errorObject.code);
+      });
+
+      */
+      this.$$('.map-panel').style.height = '100%';
+
+  },
+  hideProgressbar: function(){
+    this.$$('.progress-panel').style.display = 'none';
+  },
+  loadMarksToMap: function(){
+    var firebaseLogin  = this.$$('firebase-login');
+    var loginUser = firebaseLogin.user;
+
+    var users = this.$$('freewater-users');
+    var marksWithUsers = [];
+
+    for(var propt in this.marks){
+        var mark = this.marks[propt];
+        mark.__firebaseKey__ = propt;
+
+        mark.createdDate = moment(mark.createdDate)
+          .format('MMMM Do YYYY, h:mm:ss a');
+        mark.confirms = changeToArray(mark.confirms);
+        mark.complaints = changeToArray(mark.complaints);
+
+        var user = users.getUser( mark.user );
+        mark.user = user;
+
+        if (loginUser){
+          mark.gaveOpinion = !userDontHaveOpinion(mark, loginUser.id);
+        }
+
+        console.log('mark.gaveOpinion', mark.gaveOpinion);
+        marksWithUsers.push( this.marks[propt] );
+
+    }
+
+    this.$$('smart-map').marks = marksWithUsers;
+  },
+  publish: function (data) {
+    var self = this;
+    var message = 'Yo have to login to publish a water point';
+
+    this.checkLogin(message).then(function(user){
+        self.publishByLoggedUser(data, user);
+      });
+  },
+  checkLogin: function (message) {
+    var self = this;
+
+    return new Promise(function(resolve){
+        var firebaseLogin  = self.$$('firebase-login');
+        firebaseLogin.message = message;
+        var loginUser = firebaseLogin.user;
+
+        if (!loginUser) {
+          firebaseLogin.showLoginDialog().
+            then(function(user){
+                console.log('User', user);
+                resolve(user);
+              },
+              function(error){
+                console.error('login failed', error);
+              }
+            );
+        }else{
+          resolve(loginUser);
+        }
+    });
+  },
+  publishByLoggedUser: function(data, user){
+    var mark;
+    console.log('data.detail', data.detail);
+
+    if (data.detail){
+      mark = data.detail;
+    }else{
+      mark = data;
+    }
+
+    mark.user = user.id;
+    this.marksdataSource.push(mark);
+    this.$$('smart-map').successRegisterMessage = 'Publish successfully';
+  }
+});
---
+++ src/smart-map/smart-map.js
@@ -0,0 +1,181 @@
+function getCountry(formattedAddress){
+  var splits = formattedAddress.split(' ');
+  return splits[ splits.length - 1 ];
+}
+
+function getDistance(pos1, pos2){
+    var R = 6372.795477598;
+    var C = Math.PI/180;
+    var lata = pos1.lat;
+    var lona = pos1.lng;
+
+    var latb = pos2.lat;
+    var lonb = pos2.lng;
+
+    var distance = 2 * R * Math.asin(
+      Math.sqrt( Math.pow ( Math.sin(C * ( lata - latb ) / 2), 2) +
+        Math.cos( C * lata ) * Math.cos(C * latb) *
+        Math.pow (Math.sin(C * ( lonb - lona) / 2 ), 2))
+    );
+
+    return Math.floor(distance * 100) / 100;
+}
+
+function getReliability(mark){
+
+  if ((mark.nConfirms + mark.nComplaints) === 0) {return '../../images/new_water_icon.png';}
+
+  var reliability =  (mark.nConfirms * 100)/ (mark.nConfirms + mark.nComplaints);
+
+  if (reliability >= 75) {return '../../images/water_icon_100.png';}
+  else if (reliability < 75 && reliability >= 50) {return 'images/water_icon_75.png';}
+  else if (reliability < 50 && reliability >= 30) {return '../../images/water_icon_50.png';}
+  else {return '../../images/water_icon_20.png';}
+}
+
+Polymer({
+  is: 'smart-map',
+  listeners: {
+    'tap': 'tap'
+  },
+  properties:{
+    marks: Array,
+    pos: Object,
+    direction: Object,
+    searchResults: Array
+  },
+  observers: [
+    'changeMark(marks, pos)',
+    'currentPosChanged(searchResults)'
+  ],
+  ready: function(){
+    console.log('smart-map ready');
+    this.fire('map-ready');
+    this.loadCurrentPos();
+  },
+  currentPosChanged: function(searchResults){
+    if (searchResults.length > 0){
+      this.pos.name = searchResults[0].name;
+      /*jshint camelcase: false */ /* option: add to .jshintrc file */
+      this.pos.formattedAddress = searchResults[0].formatted_address;
+
+      var oldCountry = this.pos.country;
+      this.pos.country = getCountry(this.pos.formattedAddress);
+
+      if (this.pos.country !== oldCountry){
+        this.fire('country-changed', {country: this.pos.country});
+      }
+    }
+  },
+  changeMark: function(marks, pos){
+    this.marks = marks;
+    this.pos =pos;
+
+    if (marks && marks.length > 0 && this.pos){
+      this.sorted = [];
+
+      for (var i = 0; i < marks.length; i++){
+        var mark = marks[i];
+
+        mark.distance = getDistance(this.pos, mark);
+        mark.nConfirms = mark.confirms ? mark.confirms.length : 0;
+        mark.nComplaints = mark.complaints ? mark.complaints.length : 0;
+        mark.opinionButtonStyle = mark.gaveOpinion ? 'color:gray;' : 'color:blue;';
+        mark.icon = getReliability(mark);
+
+        this.sorted.push(mark);
+      }
+
+      this.sorted.sort( function (a, b) { return a.distance - b.distance; });
+
+      this.closer = this.sorted[0].distance < 20;
+      this.veryCloser = this.sorted[0].distance < 0.5;
+
+    }
+  },
+  loadCurrentPos: function(){
+    if (navigator.geolocation) {
+
+        setTimeout(this.loadCurrentPos.bind(this), 10000);
+        return navigator.geolocation.getCurrentPosition(
+                this.updateCurrentPosMarker.bind(this),
+                function(error){console.log(error);},
+                {enableHighAccuracy:true});
+    } else {
+      console.log('Geolocation is not supported.');
+      throw  'Geolocation is not supported.';
+    }
+  },
+  updateCurrentPosMarker: function(location){
+    console.log('your position', location);
+
+    this.pos = {
+      lat: location.coords.latitude,
+      lng: location.coords.longitude
+    };
+
+
+    this.initMap(location);
+
+    this.$.me.latitude = this.pos.lat;
+    this.$.me.longitude = this.pos.lng;
+    this.$.me.addEventListener('google-map-marker-open', this.publish.bind(this));
+
+    var googleSearch = this.$$('google-map-search');
+    googleSearch.query = this.pos.lat + ',' + this.pos.lng;
+    googleSearch.search();
+
+
+  },
+
+  initMap: function(location) {
+    //this.$.mymap = document.querySelector('google-map');
+    this.$.mymap.zoom = 17;
+    this.$.mymap.latitude = this.pos.lat;
+    this.$.mymap.longitude = this.pos.lng;
+    this.loadCurrentPos();
+  },
+  tap: function(e){
+    if (e.target.alt === 'confirm'){
+      this.fire('confirm', { mark: this.marks[ this.getIndex(e.target) ] });
+    }else if (e.target.alt === 'complaint'){
+      this.fire('complaint', { mark: this.marks[ this.getIndex(e.target) ] });
+    }else if(e.target.alt === 'directions'){
+      this.markDirectionTo(this.marks[ this.getIndex(e.target) ]);
+    }else if (e.target.alt === 'search closer'){
+      this.markDirectionTo(this.sorted[ 0 ]);
+    }else{
+      this.$$('google-map-directions').directionsRenderer.setMap(null);
+    }
+  },
+  publish: function(){
+    console.log('this.veryCloser', this.veryCloser);
+    if (!this.veryCloser){
+      this.fire('publish', {lat: this.pos.lat,
+        lng: this.pos.lng,
+        name: this.pos.name,
+        formattedAddress: this.pos.formattedAddress,
+        confirms: [],
+        complaints: [],
+        cretedDate: firebase.database.ServerValue.TIMESTAMP,
+        country: this.pos.country});
+
+        this.veryCloser = true;
+    }
+  },
+  markDirectionTo: function(mark){
+    this.$$('google-map-directions').directionsRenderer.setMap(this.$$('google-map').map);
+
+    console.log('markDirectionTo');
+    var start = this.pos.lat + ', '+ this.pos.lng;
+    var end = mark.lat + ', '+ mark.lng;
+
+    this.direction = {start: start, end: end};
+    console.log('this.direction', JSON.stringify(this.direction));
+  },
+  getIndex: function(target){
+    var parent = target.parentElement.parentElement;
+    var hiddenElement = parent.querySelector('input[type="hidden"]');
+    return hiddenElement.value;
+  }
+});
