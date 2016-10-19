fb5986e803e8b733d7c72679d3730fb9acf397ce
Dayana <dayanamcc@gmail.com>
2016-10-19 12:56:37 +0200

    Suites for Testing Auth
    Suites for Testing Auth
    all test in failing as BDD concept.
    ready to run the implementation , second cycle.

---
+++ test/bdd/auth.js
@@ -0,0 +1,64 @@
+
+describe('User Authentication', function() {
+
+
+  beforeEach('Background:Given the user has an account on Gmail or Facebook.', function() {
+      this.skip();
+  });
+    context('scenario: suceess', function() {
+
+      it('Given the user login into free-water', function() {
+
+      });
+
+      it('When user put correct email', function() {
+
+      });
+
+      it('And password', function() {
+
+      });
+
+      it('And user click on login', function() {
+
+      });
+
+      it('Then the user should be authenticated', function() {
+
+      });
+
+      it('And user should redirect to their dashboard-screen', function() {
+
+      });
+
+    });
+
+    context('scenario: bad credencials', function() {
+
+      it('Given the user login into free-water', function() {
+
+      });
+
+      it('When user put incorrect email', function() {
+
+      });
+
+      it('And password', function() {
+
+      });
+
+      it('And user click on login', function() {
+
+      });
+
+      it('Then the user is advice of bad credentials', function() {
+
+      });
+
+      it('And user the screen remains', function() {
+
+      });
+
+    });
+
+});
